92ee176a85290481b96b2616c41e4407bb05ab34
Etienne12345678910 <varlet.etienne321@gmail.com>
2023-01-17 20:42:24 +0100

    modif

--- test.py
+++ test.py
@@ -17,30 +17,34 @@
 #retravailler le code pour mettre d abord celui qui a le plus de chance par exemple 35 ans
 #essayez boucle
 
+import random
+
+
 for i in [0, 2]:
 
-    print('Entrez votre prenom:')
-    prenom = input()
-    print('Entrez votre nom:')
-    nom = input()
-    print('Entrez votre age:')
-    age = int(input())
-    print('Bonjour, ' + prenom + ' ' + nom + ' ' + str(age))
+ print('Entrez votre prenom:')
+ prenom = input()
+ print('Entrez votre nom:')
+ nom = input()
+ print('Entrez votre age:')
+ #age = int(input())
+ age = random.randint(1, 100)
+ print('Bonjour, ' + prenom + ' ' + nom + ' ' + str(age))
 
-    if  age==9 or age==10 :
-        print('Vous etes un poussin')
-    elif age==11 or age==12 :    
-     print('Vous etes un  benjamin')
-    elif age==13 or age==14 :    
-     print('Vous etes un  minime')    
-    elif age==15 or age==16 :    
+ if  age==9 or age==10 :
+  print('Vous etes un poussin')
+ elif age==11 or age==12 :    
+  print('Vous etes un  benjamin')
+ elif age==13 or age==14 :    
+    print('Vous etes un  minime')    
+ elif age==15 or age==16 :    
      print('Vous etes un  cadet')
-    elif age==17 or age==18 :    
+ elif age==17 or age==18 :    
      print('Vous etes un  junior')
-    elif age>=19 and age<=34 :    
+ elif age>=19 and age<=34 :    
      print('Vous etes un  senior') 
 
-    else :
+ else :
      print('Votre age n est pas compris entre 9 et 34 ans')
 
 
