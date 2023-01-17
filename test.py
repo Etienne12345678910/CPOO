b8e79b77a57435406a55547eb48eb2abdb77faf2
Etienne12345678910 <varlet.etienne321@gmail.com>
2023-01-17 19:28:16 +0100

    modif

---
+++ test.py
@@ -0,0 +1,28 @@
+
+
+
+#print('Hello world')
+
+
+
+#print('Salut les gars')
+#ma_variable = 'hello les gars'
+#print(type(ma_variable))
+
+#ma_variable = 3
+#print(type(ma_variable))
+
+#ma_variable = 'hello'
+#ma_variable = ma_variable + 'world'
+
+#print('toto')
+
+
+
+
+
+
+
+
+
+
