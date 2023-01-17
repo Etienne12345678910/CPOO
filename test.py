c702b0731206be793c1d5009952a66f596834c0e
Etienne12345678910 <varlet.etienne321@gmail.com>
2023-01-17 19:52:43 +0100

    modif

--- test.py
+++ test.py
@@ -22,7 +22,21 @@ print('Entrez votre age:')
 z = input()
 print('Bonjour, ' + x + ' ' + y + ' ' + z)
 
+if z=='9' or z=='10':
+    print('Vous etes un poussin')
+elif z=='11' or z=='12' :    
+    print('Vous etes un  benjamin')
+elif z=='13' or z=='14' :    
+    print('Vous etes un  minime')    
+elif z=='15' or z=='16' :    
+    print('Vous etes un  cadet')
+elif z=='17' or z=='18' :    
+    print('Vous etes un  junior')
+elif z>='19' or z<='34' :    
+    print('Vous etes un  senior')            
 
+else :
+    print('Vous etes vieux')
 
 
 
