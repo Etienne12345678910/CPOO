6c2520100165927dd739898b9bbb136c6b1c9eb9
Etienne12345678910 <varlet.etienne321@gmail.com>
2023-01-17 19:40:17 +0100

    modif

--- test.py
+++ test.py
@@ -1,25 +1,30 @@
 
-
+#test python
 
 #print('Hello world')
-
-
-
 #print('Salut les gars')
 #ma_variable = 'hello les gars'
 #print(type(ma_variable))
-
 #ma_variable = 3
 #print(type(ma_variable))
-
 #ma_variable = 'hello'
 #ma_variable = ma_variable + 'world'
-
 #print('toto')
 
-print('Enter your name:')
+#ennonce numero 1
+#demandez l age etc etc
+
+print('Entrez votre prenom:')
 x = input()
-print('Hello, ' + x)
+print('Entrez votre nom:')
+y = input()
+print('Entrez votre age:')
+z = input()
+print('Bonjour, ' + x + ' ' + y + ' ' + z)
+
+
+
+
 
 
 
