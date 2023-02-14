7abbfba4339f136c1644b2be94933409abc906ae
Etienne12345678910 <varlet.etienne321@gmail.com>
2023-02-14 19:22:36 +0100

    c mieux

--- class_a_realiser.py
+++ class_a_realiser.py
@@ -12,22 +12,35 @@
 
 class Palindrome:
 
-    def __bool__(self,chaineCara):
-        if chaineCara == chaineCara[::-1]:
+    toto='toto'
+
+    def __init__(self,phrase,toto):
+       self.phrase = phrase
+       self.phrase = toto   
+
+    def __isPalindrome__(self):
+        if self.phrase == self.phrase[::-1]:
             print('C un palindrome')
             return True
         else:
             print('C pas un palin')
             return False
-    
 
-  
+    def __del__(self):
+    # actions 
+       lala = self.toto.upper()
+       print(lala)    
+
+
 
 # ete="ete"
 # instance = Palindrome().__bool__(ete)
 # print(instance)
 
-albert="albert"
-instance = Palindrome().__bool__(albert)
-print(instance)
+# albert="albert"
+# instance = Palindrome().__isPalindrome__(albert)
+# print(instance)
+
+instance = Palindrome('albert','toto')
+print(instance.__isPalindrome__())
 
